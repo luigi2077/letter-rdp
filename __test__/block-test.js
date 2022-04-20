854e2404e731d820bef74fe337b7d68b5f120b5e
luigi2021 <chenyuan_hu@outlook.com>
2022-04-20 23:17:59 +0800

    test: recurison block

--- __test__/block-test.js
+++ __test__/block-test.js
@@ -44,5 +44,42 @@ module.exports = test => {
             }
         ]
     }
+    ),
+    test(`
+    {
+        42;
+        {
+            'hello';
+        }
+    }`,
+    {
+        type: 'Program',
+        body: [
+            {
+                type: 'BlockStatement',
+                body: [
+                    {
+                        type: 'ExpressionStatement',
+                        expression: {
+                            type: 'NumericLiteral',
+                            value: 42,
+                        }
+                    },
+                    {
+                        type: 'BlockStatement',
+                        body: [
+                            {
+                                type: 'ExpressionStatement',
+                                expression: {
+                                    type: 'StringLiteral',
+                                    value: 'hello'
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
     )
 }
